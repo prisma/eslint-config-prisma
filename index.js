1a59e6924127a7351f76a139c819ff68e1cc6be3
Jason Kuhrt <jasonkuhrt@me.com>
2024-04-10 21:28:15 -0400

    fix: deprecation plugin not 9.x ready

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
+import x from '@typescript-eslint/parser'
 import eslint from '@eslint/js'
 import tsEslint from 'typescript-eslint'
-import pluginPreferArrow from 'eslint-plugin-prefer-arrow'
-import pluginOnlyWarn from 'eslint-plugin-only-warn'
-import pluginTsdoc from 'eslint-plugin-tsdoc'
-import pluginDeprecation from 'eslint-plugin-deprecation'
+import preferArrow from 'eslint-plugin-prefer-arrow'
+import onlyWarn from 'eslint-plugin-only-warn'
+import tsdoc from 'eslint-plugin-tsdoc'
+import deprecation from 'eslint-plugin-deprecation'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tsEslint.config(
   eslint.configs.recommended,
   // https://github.com/prettier/eslint-config-prettier
   eslintConfigPrettier,
-  // This enables the use of a lint rule to reject function declarations. This is
-  // preferable as a way to encourage higher order function usage. For example it is not
-  // possible to wrap a function declaration with Open Telemetry instrumentation but it is
-  // possible to wrap an arrow function since its an expression.
-  // https://github.com/TristonJ/eslint-plugin-prefer-arrow
-  pluginPreferArrow,
-  // This makes it so the IDE reports lint rejections as warnings only. This is
-  // better than errors because most lint rejections are not runtime errors. This
-  // allows IDE errors to be exclusive for e.g. static type errors which often are
-  // reflective of real runtime errors.
-  // https://github.com/bfanger/eslint-plugin-only-warn
-  pluginOnlyWarn,
-  // https://github.com/microsoft/tsdoc/tree/master/eslint-plugin
-  pluginTsdoc,
-  // This enables the use of a lint rule to reject use of @deprecated functions.
-  // https://github.com/gund/eslint-plugin-deprecation
-  pluginDeprecation,
   // https://github.com/typescript-eslint/typescript-eslint
   ...tsEslint.configs.strictTypeChecked,
   {
+    name: 'prisma',
+    plugins: {
+      // This enables the use of a lint rule to reject function declarations. This is
+      // preferable as a way to encourage higher order function usage. For example it is not
+      // possible to wrap a function declaration with Open Telemetry instrumentation but it is
+      // possible to wrap an arrow function since its an expression.
+      // https://github.com/TristonJ/eslint-plugin-prefer-arrow
+      'prefer-arrow': preferArrow,
+      // This enables the use of a lint rule to reject use of @deprecated functions.
+      // https://github.com/gund/eslint-plugin-deprecation
+      deprecation,
+      // https://github.com/microsoft/tsdoc/tree/master/eslint-plugin
+      tsdoc,
+      // This makes it so the IDE reports lint rejections as warnings only. This is
+      // better than errors because most lint rejections are not runtime errors. This
+      // allows IDE errors to be exclusive for e.g. static type errors which often are
+      // reflective of real runtime errors.
+      // https://github.com/bfanger/eslint-plugin-only-warn
+      onlyWarn,
+    },
+    languageOptions: {
+      parser: x,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
     rules: {
       '@typescript-eslint/consistent-type-imports': 'warn',
       // Enforce backticks
@@ -37,8 +47,8 @@ export default tsEslint.config(
       quotes: 'off',
       '@typescript-eslint/quotes': ['warn', 'backtick'],
       'tsdoc/syntax': 'warn',
-      'simple-import-sort/exports': 'warn',
-      'deprecation/deprecation': 'warn',
+      // TODO enable this rule once https://github.com/gund/eslint-plugin-deprecation/issues/78
+      // 'deprecation/deprecation': 'warn',
       'prefer-arrow/prefer-arrow-functions': 'warn',
       // TypeScript makes these safe & effective
       'no-case-declarations': 'off',
